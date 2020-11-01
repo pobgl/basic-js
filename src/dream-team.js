25e0f64c0681920416d4022e42291583cb68ce6a
Gleb Pobezhimov <pglb@yandex.ru>
2020-11-02 00:11:59 +0400

    feat: add dream-team task solution;

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if (members instanceof Array || members != null || members != undefined) {
+    let  result = '';
+
+    for (let i=0; i < members.length; i++) {
+      if (typeof members[i] === 'string') {
+        result += members[i].toUpperCase().trim()[0]; 
+      }
+    }  
+    return result.split('').sort().join('');    
+  } else {
+    return false;    
+  }
 };

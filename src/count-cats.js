e01045ba99e3094d1b6c74fd1098830d08e97098
Gleb Pobezhimov <pglb@yandex.ru>
2020-11-01 12:49:50 +0400

    feat: add count-cats and hanoi-tower solution;

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,7 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
-  
+
+  let catsNumber = 0;
+
+  for(let i of matrix) {
+    for (let n of i) {
+      if (n == '^^') {
+        catsNumber++
+      }
+    }      
+  }
+  return catsNumber;
 };

548dfb4e4af616ce8e8526dbc8fea5abd6919e03
Gleb Pobezhimov <pglb@yandex.ru>
2020-11-01 16:28:52 +0400

    feat: add carbon-dating and extended-repeater solution;

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+
+  if (!sampleActivity || typeof(sampleActivity) !== 'string') {
+    return false;
+  }
+
+  const sampleNumber = parseFloat(sampleActivity);
+    if (sampleNumber <= 0 || sampleNumber > MODERN_ACTIVITY || isNaN(sampleNumber)) {
+  return false;
+  } return Math.ceil(Math.log(MODERN_ACTIVITY / sampleNumber) /  0.693 * HALF_LIFE_PERIOD);
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
+module.exports = function repeater(str, options) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
-};
+  
+  let {additionSeparator = '|', addition = '', separator = '+',
+  repeatTimes, additionRepeatTimes} = options;
+  let separatorBlock = addition;
+  let result = '';  
+  
+  for (let i = 1; i < additionRepeatTimes; i++) {
+    separatorBlock += `${additionSeparator}${addition}`;
+  }
+
+  for (let i = 1; i < repeatTimes; i++) {
+    result += `${separator}${str}${separatorBlock}`;
+  }
+
+  return `${str}${separatorBlock}${result}`;  
+}
   
